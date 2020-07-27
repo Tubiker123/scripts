7a6e0565bd4f50aedc5028291293694197f15104
z4gs <54650179+z4gs@users.noreply.github.com>
2020-07-27 18:53:24 -0300

    Update elerium ui lib v2.lua

--- elerium ui lib v2.lua
+++ elerium ui lib v2.lua
@@ -12,7 +12,7 @@ local ui_options = {
 }
 
 do
-	local imgui = game:GetService("CoreGui"):FindFirstChild("imgui")
+	local imgui = game:GetService("CoreGui"):FindFirstChild(imgui)
 	if imgui then imgui:Destroy() end
 end
 
