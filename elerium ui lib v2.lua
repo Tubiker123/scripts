4e0c99cfd0c22fd08d8a91d4f9eba0831fbbe01e
z4gs <54650179+z4gs@users.noreply.github.com>
2020-08-21 23:15:25 -0300

    Update elerium ui lib v2.lua

--- elerium ui lib v2.lua
+++ elerium ui lib v2.lua
@@ -4,13 +4,6 @@
 	by Singularity (V3rm @ King Singularity) (Discord @ Singularity#5490)
 --]]
 
-local ui_options = {
-	main_color = Color3.fromRGB(41, 74, 122),
-	min_size = Vector2.new(400, 300),
-	toggle_key = Enum.KeyCode.RightShift,
-	can_resize = true,
-}
-
 local imgui = Instance.new("ScreenGui", workspace)
 pcall(function() syn.protect_gui(imgui) end)
 imgui.DisplayOrder = 1
