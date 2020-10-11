b2707712d1ff1826847e5ad9d7b732d132bdb924
z4gs <54650179+z4gs@users.noreply.github.com>
2020-10-10 21:20:16 -0300

    Update ShinobiLife2_questfarm.lua

--- ShinobiLife2_questfarm.lua
+++ ShinobiLife2_questfarm.lua
@@ -26,7 +26,7 @@ local function getQuest()
     for i,v in next, workspace.missiongivers:children() do
         if v.Name == "" and v.Head.givemission.Enabled and v.Head.givemission.color.Image == "http://www.roblox.com/asset/?id=5459241648" then -- get defeat npc missions
             return v
-        elseif v.Name == "" and v.Head.givemission.Enabled and v.Head.givemission.color.Image == "http://www.roblox.com/asset/?id=5459241799" and player.statz.lvl.lvl.Value >= 700 then -- get defeat boss missions if your level >= 700
+        elseif v.Name == "" and v.Head.givemission.Enabled and v.Head.givemission.color.Image == "http://www.roblox.com/asset/?id=5459241799" and player.statz.lvl.lvl.Value >= 700 then -- get defeat boss missions if your level >= 70
             return v
         end
     end
@@ -37,27 +37,28 @@ player.Idled:connect(function()
 end)
 
 while getgenv().options.autofarm do
-    if not player.currentmission.Value then
-        mobmission = getQuest()
-        if mobmission then
-            repeat
-                player.Character.HumanoidRootPart.CFrame = mobmission.HumanoidRootPart.CFrame
-                mobmission.CLIENTTALK:FireServer()
-                mobmission.CLIENTTALK:FireServer("accept")
-                heartbeat:wait() 
-            until player.currentmission.Value
-            mobmission = nil
-        end
-    end
     pcall(function()
-        for i,v in next, workspace.npc:children() do
-            if v.Name ~= "logtraining" and v.HumanoidRootPart:FindFirstChild("BLOCKBAR") then
-                while v.Humanoid.Health > 0 do
-                    for i = 0, 20 do
-                        v.Humanoid.Health = 0
+        if not player.currentmission.Value then
+            mobmission = getQuest()
+            if mobmission then
+                repeat
+                    player.Character.HumanoidRootPart.CFrame = mobmission.HumanoidRootPart.CFrame
+                    mobmission:WaitForChild("CLIENTTALK"):FireServer()
+                    mobmission:WaitForChild("CLIENTTALK"):FireServer("accept")
+                    heartbeat:wait() 
+                until player.currentmission.Value
+                mobmission = nil
+            end
+        else
+            for i,v in next, workspace.npc:children() do
+                if v.Name ~= "logtraining" and v.HumanoidRootPart:FindFirstChild("BLOCKBAR") then
+                    while v.Humanoid.Health > 0 do
+                        for i = 0, 20 do
+                            v.Humanoid.Health = 0
+                        end
+                        player.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
+                        wait()
                     end
-                    player.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame
-                    wait()
                 end
             end
         end
