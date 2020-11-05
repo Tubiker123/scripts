b2a773b9788f2a6330689d646003e35e7aa2d2bf
z4gs <54650179+z4gs@users.noreply.github.com>
2020-11-05 12:19:52 -0300

    Update FlameZero-autofarm.lua

--- FlameZero-autofarm.lua
+++ FlameZero-autofarm.lua
@@ -3,7 +3,7 @@ loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/z4gs/scripts/mas
 local player = game:GetService("Players").LocalPlayer
 local heartbeat = game:GetService("RunService").Heartbeat
 local remotes = game:GetService("ReplicatedStorage").Remotes
-local targ, quest, btn, farm, weap, run, newindex
+local btn, run, newindex
 
 local gui = library:AddWindow("Flame Zero Auto Farm", {
     main_color = Color3.fromRGB(255, 69, 0),
@@ -11,7 +11,7 @@ local gui = library:AddWindow("Flame Zero Auto Farm", {
     can_resize = false
 })
 
-local npcs = {
+local array = {
     ["Bandit"] = "Allen",
     ["Sadistic Pyromancer"] = "Bertrum",
     ["Thug"] = "Hadja",
@@ -32,18 +32,12 @@ local npcs = {
 local tab = gui:AddTab("Main")
 
 tab:AddLabel("NPC")
-local drop = tab:AddDropdown("Select", function(opt)
-    targ, quest = opt, npcs[opt]
-end)
+local drop = tab:AddDropdown("Select", function(opt) array.target, array.quest = opt, npcs[opt] end)
 
-for i,v in next, npcs do
-    drop:Add(i) 
-end
+for i,v in next, array do drop:Add(i) end
 
 tab:AddLabel("Weapon")
-local drop2 = tab:AddDropdown("Select", function(opt)
-    weap = opt
-end)
+local drop2 = tab:AddDropdown("Select", function(opt) array.weap = opt end)
 
 for i,v in next, player.Backpack:children() do 
     if v.ClassName == "Tool" and not v.Name:match("Gen") then 
@@ -52,13 +46,13 @@ for i,v in next, player.Backpack:children() do
 end
 
 btn = tab:AddButton("Start", function()
-    if not farm then
+    if not array.farm then
         run = game:GetService("RunService").Stepped:connect(function()
             pcall(function() player.Character.Humanoid:ChangeState(11) end)
         end)
-        farm = true btn.Text = "Stop"
+        array.farm = true btn.Text = "Stop"
     else
-        farm = false btn.Text = "Start"
+        array.farm = false btn.Text = "Start"
         run:Disconnect()
     end
 end)
@@ -81,7 +75,7 @@ end))
 
 spawn(function()
     while wait() do
-        if farm and player.PlayerFolder.QuestMax.Value == 0 then
+        if array.farm and player.PlayerFolder.QuestMax.Value == 0 then
             pcall(function()
                 player.Character.HumanoidRootPart.CFrame = workspace.NPCS[quest].HumanoidRootPart.CFrame
                 remotes.Player.NPCChat:FireServer("Re-Do", quest)
@@ -91,13 +85,13 @@ spawn(function()
 end)
 
 while wait() do
-    if farm then
+    if array.farm then
         pcall(function()
             for i,v in next, workspace.FightableNPCS:children() do
-                if v.Name == targ then
+                if v.Name == array.target then
                     while v.Humanoid.Health > 0 and farm and player.Character.Humanoid.Health > 0 do
                         player.Character.HumanoidRootPart.CFrame = (v.HumanoidRootPart.CFrame * CFrame.new(0,5,0)) * CFrame.Angles(80,0,0)
-                        if weap == "Combat" then
+                        if array.weap == "Combat" then
                             remotes.Melee.Combat:FireServer("Punch", 1)
                         else
                             remotes.Melee.Sword:FireServer(weap, "Slash", 1)
