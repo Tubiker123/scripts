a576ca1d18465914203484feafbc605a99a8f319
z4gs <54650179+z4gs@users.noreply.github.com>
2020-11-06 00:58:20 -0300

    Update ShinobiLife2_questfarm.lua

--- ShinobiLife2_questfarm.lua
+++ ShinobiLife2_questfarm.lua
@@ -19,7 +19,7 @@ local player = game:GetService("Players").LocalPlayer
 local heartbeat = game:GetService("RunService").Heartbeat
 local mobmission
 
-hookfunction(getrenv().print, newcclosure(function() end))
+game:GetService("ScriptContext"):SetTimeout(.1)
 
 local function getQuest()
     for i,v in next, workspace.missiongivers:children() do
