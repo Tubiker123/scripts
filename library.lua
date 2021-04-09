a60e22112d74ee76b6eb52569be20179b5941820
z4gs <54650179+z4gs@users.noreply.github.com>
2021-04-08 21:17:54 -0300

    Delete library.lua

--- library.lua
+++
@@ -1,3 +0,0 @@
-setclipboard('loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/z4gs/scripts/master/Ro-Ghoul%20Auto%20Farm.lua"))()')
-game.Players.LocalPlayer:Kick("stop using the outdated version of the script, the updated script has been copied to your clipboard")
-return wait(math.huge)
